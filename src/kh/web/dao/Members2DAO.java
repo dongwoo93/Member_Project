4241fbd6731bd987f6edbb7269dbdc9f2513ddb0
임동철 <star379812@gmail.com>
2018-06-14 16:31:33 +0900

    Members2DAO 메소드 추가
    idCheck , login 메소드 생성

--- src/kh/web/dao/Members2DAO.java
+++ src/kh/web/dao/Members2DAO.java
@@ -1,9 +1,55 @@
 package kh.web.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.InitialContext;
 
-public class Members2DAO {
+import kh.web.dbutils.DBUtils;
 
+public class Members2DAO {
+	public boolean idCheck(String id) throws Exception{
+		boolean b = false;
+		
+		Connection con = DBUtils.getConnection();
+		String sql = "select * from MEMBERS2 where ID=?";
+		
+		PreparedStatement pstat = con.prepareStatement(sql);
+		
+		pstat.setString(1, id);
+		
+		ResultSet rs = pstat.executeQuery();
+		
+		while(rs.next()) {
+			if(rs.getString(1).equals(id)) {
+				b=true;
+			}
+		}
+		
+		return b;
+	}
+	
+	public boolean login(String id, String pw) throws Exception{
+		boolean b = false;
+		
+		Connection con = DBUtils.getConnection();
+		
+		String sql = "select * from MEMBERS2 where ID=?,PW=?";
+		
+		PreparedStatement pstat = con.prepareStatement(sql);
+		
+		pstat.setString(1, id);
+		pstat.setString(2, pw);
+		
+		ResultSet rs = pstat.executeQuery();
+		
+		while(rs.next()) {
+			if(rs.getString(1).equals(id) && rs.getString(2).equals(pw)) {
+				b = true;
+			}
+		}
+		
+		return b;
+	}
 }
